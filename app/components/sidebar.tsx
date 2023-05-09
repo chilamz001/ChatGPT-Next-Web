a4d7d02b4c6dffc0a322c914fd194e951513099c
zhangzl <zhangzl@qsign.cn>
2023-05-10 01:10:13 +0800

    修改界面

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -105,7 +105,7 @@ export function SideBar(props: { className?: string }) {
         </div>
       </div>
 
-      <div className={styles["sidebar-header-bar"]}>
+      {/*<div className={styles["sidebar-header-bar"]}>
         <IconButton
           icon={<MaskIcon />}
           text={shouldNarrow ? undefined : Locale.Mask.Name}
@@ -120,7 +120,7 @@ export function SideBar(props: { className?: string }) {
           onClick={() => showToast(Locale.WIP)}
           shadow
         />
-      </div>
+      </div>*/}
 
       <div
         className={styles["sidebar-body"]}
